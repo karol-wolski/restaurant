a54c4461acc09b4d4e67d5ff85f8893e451a7d5b
Karol Wolski <karolwolski04@gmail.com>
2020-07-07 15:09:46 +0200

    navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 import { initialMenuCard, filterMenu } from './menu-card.js'
+import { mobileMenu } from './mobile-menu.js'
 
 const initial = () => {
+  const hamburger = document.querySelector('#menu-toggle')
+  hamburger.addEventListener('click', mobileMenu)
   initialMenuCard()
   filterMenu()
 }
