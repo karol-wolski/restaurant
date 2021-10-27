2be60f207b97518d1b5bebaac0d87e4c4d665d07
Karol Wolski <karolwolski04@gmail.com>
2021-10-27 13:11:32 +0200

    feat: Contact form validation
    Add constatns
    Add messages
    Add helpers
    Add alert component
    Add form and field validation

---
+++ src/js/messages.js
@@ -0,0 +1,9 @@
+const messages = {
+  REQUIRED_ERR_MSG: 'Field is required.',
+  ONLY_LETTERS_ERR_MSG: 'Field should have only letters.',
+  PHONE_ERR_MSG: 'Field should have format: 500100500.',
+  EMAIL_ERR_MSG: 'Field should have format: john@doe.com.',
+  SUCCESSFULLY_SEND_MSG: 'Your e-mail has been successfully sent.',
+}
+
+export { messages }
--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ import { initialMenuCard, filterMenu } from './menu-card.js'
 import { mobileMenu } from './mobile-menu.js'
 import { bookButton } from './book-table.js'
 import { scrollToSection } from './scrollToSection.js'
+import { contactForm } from './contact-form.js'
 
 const initial = () => {
   const hamburger = document.querySelector('#menu-toggle')
@@ -10,6 +11,7 @@ const initial = () => {
   filterMenu()
   bookButton()
   scrollToSection()
+  contactForm()
 }
 
 initial()
